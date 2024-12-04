c6df49ee94ae603a19d07825831f3b66b4387516
kosakinha012 <davikosaka20@gmail.com>
2024-12-04 10:48:42 -0300

    subindo frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [userEffectCounter, setUseEffctCounter] = useState(0)
+
+
+  const [logado, setLogado] = useState(false);
+
+  useEffect(() => {
+    console.log("useeffect chamado");
+    setUseEffctCounter(userEffectCounter + 1);
+  }, [logado])
+
+  const logar = () => {
+    setLogado(true);
+  };
+
+  const deslogar = () => {
+    setLogado(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>use effect foi chamado {userEffectCounter} vezes</h1>
+      {logado ? <p> logado </p> : <p>deslogado</p>}
+      <button onClick={logar}>logar</button>
+      <button onClick={deslogar}>deslogar</button>
     </div>
   );
 }
